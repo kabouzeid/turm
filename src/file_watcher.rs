9f954ccff53fc7ffdb4412d1a490ef012bf4cc95
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2023-03-21 11:04:49 +0100

    feat: faster log file loading

--- src/file_watcher.rs
+++ src/file_watcher.rs
@@ -13,10 +13,16 @@ use notify::{event::ModifyKind, RecursiveMode, Watcher};
 
 use crate::app::AppMessage;
 
+struct FileReader {
+    content_sender: Sender<Option<String>>,
+    receiver: Receiver<()>,
+    file_path: Option<PathBuf>,
+}
+
 struct FileWatcher {
     app: Sender<AppMessage>,
     receiver: Receiver<FileWatcherMessage>,
-    file_path: Option<notify::Result<PathBuf>>,
+    file_path: Option<PathBuf>,
 }
 pub enum FileWatcherMessage {
     FilePath(Option<PathBuf>),
@@ -49,38 +55,78 @@ impl FileWatcher {
         })
         .unwrap();
 
+        let (mut content_sender, mut content_receiver) = unbounded::<Option<String>>();
+        let (mut _watch_sender, mut _watch_receiver) = unbounded::<()>();
         loop {
             select! {
                 recv(self.receiver) -> msg => {
                     match msg.unwrap() {
                         FileWatcherMessage::FilePath(file_path) => {
-                            if let Some(Ok(p)) = &self.file_path {
+                            (content_sender, content_receiver) = unbounded();
+                            (_watch_sender, _watch_receiver) = unbounded::<()>();
+
+                            if let Some(p) = &self.file_path {
                                 watcher.unwatch(p).expect(format!("Failed to unwatch {:?}", p).as_str());
                                 self.file_path = None;
                             }
-                            self.file_path = file_path.map(|p| watcher.watch(Path::new(&p), RecursiveMode::NonRecursive).map(|_| p));
+
+                            if let Some(p) = file_path {
+                                let res = watcher.watch(Path::new(&p), RecursiveMode::NonRecursive);
+                                match res {
+                                    Ok(_) => {
+                                        self.file_path = Some(p);
+                                        let p = self.file_path.clone();
+                                        thread::spawn(move || FileReader::new(content_sender, _watch_receiver, p).run());
+                                    },
+                                    Err(e) => self.app.send(AppMessage::JobStdout(Some(format!("Failed to watch {:?}: {}", p, e)))).unwrap()
+                                };
+                            }
                         }
                     }
                 }
-                recv(watch_receiver) -> _ => {}
-                // in case the file watcher doesn't work (e.g. network mounted fs)
-                default(Duration::from_secs(5)) => {}
+                recv(watch_receiver) -> _ => { _watch_sender.send(()).unwrap(); }
+                recv(content_receiver) -> msg => {
+                    self.app.send(AppMessage::JobStdout(msg.unwrap())).unwrap();
+                }
             }
-            self.update();
+        }
+    }
+}
+
+impl FileReader {
+    fn new(
+        content_sender: Sender<Option<String>>,
+        receiver: Receiver<()>,
+        file_path: Option<PathBuf>,
+    ) -> Self {
+        FileReader {
+            content_sender: content_sender,
+            receiver: receiver,
+            file_path: file_path,
         }
     }
 
-    fn update(&self) {
-        let s = self.file_path.as_ref().and_then(|file_path| {
-            match file_path {
-                Ok(p) => {
-                    // TODO: partial read only
-                    fs::read_to_string(p).ok().map(|s| s.replace("\r", "\n")) // .replace doesn't really belong here, but it can be slow because log files can be huge => so better to do it here (off the UI thread) for now
+    fn run(&self) -> Result<(), ()> {
+        loop {
+            self.update().map_err(|_| ())?;
+            select! {
+                recv(self.receiver) -> msg => {
+                    msg.map_err(|_| ())?;
                 }
-                Err(e) => Some(format!("Error: {}", e)),
+                // in case the file watcher doesn't work (e.g. network mounted fs)
+                default(Duration::from_secs(10)) => {}
             }
+        }
+    }
+
+    fn update(&self) -> Result<(), crossbeam::channel::SendError<Option<String>>> {
+        let s = self.file_path.as_ref().and_then(|file_path| {
+            // TODO: partial read only
+            fs::read_to_string(file_path)
+                .ok()
+                .map(|s| s.replace("\r", "\n")) // .replace doesn't really belong here, but it can be slow because log files can be huge => so better to do it here (off the UI thread) for now
         });
-        self.app.send(AppMessage::JobStdout(s)).unwrap();
+        self.content_sender.send(s)
     }
 }
 
